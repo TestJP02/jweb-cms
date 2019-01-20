d3e4166e26e079f57b094dda91ab1523c2e6cd2e
chi <chiron.chi@gmail.com>
2019-01-20 12:10:53 +0800

    skip install process

--- jweb-main/src/main/java/Main.java
+++ jweb-main/src/main/java/Main.java
@@ -1,25 +1,27 @@
-import app.jweb.lancher.Launcher;
+import app.jweb.AbstractModule;
+import app.jweb.undertow.UndertowApp;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ServiceLoader;
 
 /**
  * @author chi
  */
 public class Main {
     public static void main(String[] args) {
-//        Path path = Paths.get(System.getProperty("user.dir")).resolve("jweb-main/src/main/dist");
-//        UndertowApp app = new UndertowApp(path);
-//        ServiceLoader.load(AbstractModule.class).forEach(app::install);
-//        app.start();
-
-        String jwebHome = System.getProperty("jweb.home");
-        Path homeDir = jwebHome == null ? Paths.get(System.getProperty("user.home")).resolve(".jweb") : Paths.get(jwebHome);
-        Launcher launcher = new Launcher(homeDir);
-        if (!launcher.isInstalled()) {
-            launcher.install();
-        } else {
-            launcher.start();
-        }
+        Path path = Paths.get(System.getProperty("user.dir")).resolve("jweb-main/src/main/dist");
+        UndertowApp app = new UndertowApp(path);
+        ServiceLoader.load(AbstractModule.class).forEach(app::install);
+        app.start();
+//
+//        String jwebHome = System.getProperty("jweb.home");
+//        Path homeDir = jwebHome == null ? Paths.get(System.getProperty("user.home")).resolve(".jweb") : Paths.get(jwebHome);
+//        Launcher launcher = new Launcher(homeDir);
+//        if (!launcher.isInstalled()) {
+//            launcher.install();
+//        } else {
+//            launcher.start();
+//        }
     }
 }
