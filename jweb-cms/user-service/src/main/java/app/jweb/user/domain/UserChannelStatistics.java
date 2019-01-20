440a128ce77481c51fd940a30c034ce725fc66be
chi <chiron.chi@gmail.com>
2019-01-20 12:41:29 +0800

    979828515297688 dashboard display total pages 979828515297687 dashboard display total users

--- jweb-cms/user-service/src/main/java/app/jweb/user/domain/UserChannelStatistics.java
+++ jweb-cms/user-service/src/main/java/app/jweb/user/domain/UserChannelStatistics.java
@@ -9,4 +9,9 @@ import javax.persistence.Entity;
 public class UserChannelStatistics {
     public String channel;
     public Long total;
+
+    public UserChannelStatistics(String channel, Long total) {
+        this.channel = channel;
+        this.total = total;
+    }
 }
